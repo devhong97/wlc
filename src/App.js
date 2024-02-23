ec9d2a3ec9f4bcd68068fb3a6aff7bfb149944b7
leeccon <lsm@delmetenz.com>
2024-02-23 17:45:17 +0900

    pubilsing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Home from "./components/Home/Home";
 import "./assets/scss/index.scss";
 import { Route, Routes } from "react-router-dom";
 import Router from "./router/Router";
+import Aside from "./components/Header/Aside";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div style={{ height: "100%" }}>
-      <div className="Screen">
+    <div className="screen">
+      <div className="menu_area">
+        <Aside></Aside>
+      </div>
+      <div className="main_area">
+        <Header></Header>
         <Router>
           <Routes>
             <Route path="/" element={<Home />} />
