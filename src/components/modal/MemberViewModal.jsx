77111f2c5327408104c48b4a39a6f115500a7068
DESKTOP-SG1016V\iccon <dlthfal93@hanmail.net>
2024-03-14 15:02:36 +0900

    commit

--- src/components/modal/MemberViewModal.jsx
+++ src/components/modal/MemberViewModal.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import moment from 'moment';
+import moment from "moment";
+import { useBranchContext } from '../Context/BranchContext';
 const MemberViewModal = (props) => {
+    const { typeGroup, companyGroup, branchGroup, setContextType, setContextCompany } = useBranchContext();
     const [memberData, setMemberData] = useState([]);
     const [id, setId] = useState("");
     const [password, setPassword] = useState("");
@@ -10,29 +12,58 @@ const MemberViewModal = (props) => {
     const [phone, setPhone] = useState("");
     const [bank, setBank] = useState("");
     const [bankAccount, setBankAccount] = useState("");
-    const [category1, setCategory1] = useState("");
-    const [category2, setCategory2] = useState("");
+    const [type, setType] = useState("");
+    const [company, setCompany] = useState("");
     const [branchName, setBranchName] = useState("");
+    const [branchIdx, setBranchIdx] = useState("");
+
+    // 분류소스
     useEffect(() => {
         if (props.detailIdx) {
             console.log(props.detailIdx);
             getDetail();
         }
-    }, [props.detailIdx])
+    }, [props.detailIdx]);
     useEffect(() => {
-        setDetailValue()
-    }, [memberData])
-    const clearModal = () => {
-        props.closeModal()
+        setContextType(type);
+    }, [type])
+    useEffect(() => {
+        setContextCompany(company);
+    }, [company])
+
+    const selectBranch = (num) => {
+        setBranchIdx(num);
+        const selectedBranch = branchGroup.find(data => data.idx === Number(num));
+        if (selectedBranch) {
+            setBranchName(selectedBranch.branch_name);
+        }
     }
+    const selectType = (data) => {
+        setType(data);
+        //지점명 초기화
+        setCompany("");
+        setBranchIdx("");
+        setBranchName("");
+    }
+    // ///////////////////
+
+    useEffect(() => {
+        setDetailValue();
+    }, [memberData]);
+
+    const clearModal = () => {
+        props.closeModal();
+    };
+
     const getDetail = async () => {
         try {
             const response = await Axios.get(
-                "http://localhost:3001/api/get/member_detail", {
-                params: {
-                    idx: props.detailIdx
+                "http://localhost:3001/api/get/member_detail",
+                {
+                    params: {
+                        idx: props.detailIdx,
+                    },
                 }
-            }
             );
             const allData = response.data;
             setMemberData(allData[0]);
@@ -40,32 +71,77 @@ const MemberViewModal = (props) => {
         } catch (error) {
             console.error("Error fetching list:", error);
         }
-    }
+    };
     const setDetailValue = () => {
-        setPassword(memberData.password);
+        // setPassword(memberData.password);
         setEmail(memberData.email);
         setPhone(memberData.phone);
         setBank(memberData.bank);
         setBankAccount(memberData.deposit_account);
-        setCategory1(memberData.company_type);
-        setCategory2(memberData.company_name);
-        setBranchName(memberData.branch);
-    }
-    const handleSubmit = () => {
+        setType(memberData.branch_type);
+        setCompany(memberData.company_name);
+        setBranchName(memberData.branch_name);
+        setBranchIdx(memberData.branch_idx)
+    };
 
-    }
+
+    const handleUpdate = async () => {
+        if (
+            !email ||
+            !phone ||
+            !bank ||
+            !bankAccount ||
+            !type ||
+            !company ||
+            !branchName ||
+            !branchIdx
+        ) {
+            alert("필수 사항을 모두 입력해주세요");
+            return;
+        }
+        const paramsArray = {
+            email: email,
+            phone: phone,
+            bank: bank,
+            deposit_account: bankAccount,
+            branch_type: type,
+            company_name: company,
+            branch_name: branchName,
+            branch_idx: branchIdx,
+            idx: props.detailIdx
+        }
+
+        // 비밀번호 수정 시에 params에 추가
+        if (password !== "") {
+            paramsArray.password = password;
+        }
+
+        try {
+            const response = await Axios.post(
+                "http://localhost:3001/api/post/member_edit", paramsArray
+
+            );
+
+            console.log(response.data);
+            props.closeModal();
+        } catch (error) {
+            console.error("Error during registration:", error);
+        }
+    };
     const deleteMember = async () => {
         try {
             const response = await Axios.post(
-                "http://localhost:3001/api/post/member_delete", {
-                idx: props.detailIdx
-            });
+                "http://localhost:3001/api/post/member_delete",
+                {
+                    idx: props.detailIdx,
+                }
+            );
             alert("삭제되었습니다.");
             props.closeModal();
         } catch (error) {
             console.error("Error fetching list:", error);
         }
-    }
+    };
     return (
         <div className="modal_wrap">
             <div className="modal_back">
@@ -79,11 +155,9 @@ const MemberViewModal = (props) => {
                     <div className="table_box">
                         <div className="table_row">
                             <div className="table_section">
-                                <div className="table_title">
-                                    가입일
-                                </div>
+                                <div className="table_title">가입일</div>
                                 <div className="table_contents w100">
-                                    <div className='table_inner_text'>{memberData.date}</div>
+                                    <div className="table_inner_text">{memberData.date}</div>
                                 </div>
                             </div>
                         </div>
@@ -93,7 +167,7 @@ const MemberViewModal = (props) => {
                                     아이디<p className="title_point">*</p>
                                 </div>
                                 <div className="table_contents w100">
-                                    <div className='table_inner_text'>{memberData.id}</div>
+                                    <div className="table_inner_text">{memberData.id}</div>
                                 </div>
                             </div>
                         </div>
@@ -108,7 +182,8 @@ const MemberViewModal = (props) => {
                                         type="password"
                                         id="title"
                                         placeholder="비밀번호를 입력해주세요."
-                                        value={password} onChange={(e) => setPassword(e.target.value)}
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
                                     ></input>
                                 </div>
                             </div>
@@ -119,7 +194,7 @@ const MemberViewModal = (props) => {
                                     이름<p className="title_point">*</p>
                                 </div>
                                 <div className="table_contents w100">
-                                    <div className='table_inner_text'>{memberData.name}</div>
+                                    <div className="table_inner_text">{memberData.name}</div>
                                 </div>
                             </div>
                         </div>
@@ -134,7 +209,8 @@ const MemberViewModal = (props) => {
                                         type="text"
                                         id="title"
                                         placeholder="이메일을 입력해주세요."
-                                        value={email} onChange={(e) => setEmail(e.target.value)}
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
                                     ></input>
                                 </div>
                             </div>
@@ -150,7 +226,8 @@ const MemberViewModal = (props) => {
                                         type="text"
                                         id="title"
                                         placeholder="연락처를 입력해주세요."
-                                        value={phone} onChange={(e) => setPhone(e.target.value)}
+                                        value={phone}
+                                        onChange={(e) => setPhone(e.target.value)}
                                     ></input>
                                 </div>
                             </div>
@@ -178,7 +255,8 @@ const MemberViewModal = (props) => {
                                         type="text"
                                         id="title"
                                         placeholder="계좌를 입력해주세요."
-                                        value={bankAccount} onChange={(e) => setBankAccount(e.target.value)}
+                                        value={bankAccount}
+                                        onChange={(e) => setBankAccount(e.target.value)}
                                     ></input>
                                 </div>
                             </div>
@@ -192,11 +270,14 @@ const MemberViewModal = (props) => {
                                     <select
                                         name="affiliation"
                                         className="table_select"
+                                        value={type} onChange={(e) => selectType(e.target.value)}
                                     >
                                         <option value="">선택</option>
-                                        <option value="company">Company</option>
-                                        <option value="school">School</option>
-                                        <option value="organization">Organization</option>
+                                        {typeGroup.map((type, index) => {
+                                            return (
+                                                <option key={index} value={type}>{type}</option>
+                                            );
+                                        })}
                                     </select>
                                 </div>
                             </div>
@@ -208,11 +289,14 @@ const MemberViewModal = (props) => {
                                     <select
                                         name="affiliation"
                                         className="table_select"
+                                        value={company} onChange={(e) => setCompany(e.target.value)}
                                     >
                                         <option value="">선택</option>
-                                        <option value="company">Company</option>
-                                        <option value="school">School</option>
-                                        <option value="organization">Organization</option>
+                                        {companyGroup.map((data, index) => {
+                                            return (
+                                                <option key={index} value={data}>{data}</option>
+                                            );
+                                        })}
                                     </select>
                                 </div>
                             </div>
@@ -226,11 +310,14 @@ const MemberViewModal = (props) => {
                                     <select
                                         name="affiliation"
                                         className="table_select"
+                                        value={branchIdx} onChange={(e) => selectBranch(e.target.value)}
                                     >
                                         <option value="">선택</option>
-                                        <option value="company">Company</option>
-                                        <option value="school">School</option>
-                                        <option value="organization">Organization</option>
+                                        {branchGroup.map((data, index) => {
+                                            return (
+                                                <option key={index} value={data.idx}>{data.branch_name}</option>
+                                            );
+                                        })}
                                     </select>
                                 </div>
                             </div>
@@ -238,27 +325,22 @@ const MemberViewModal = (props) => {
 
                         <div className="table_row">
                             <div className="table_section half">
-                                <div className="table_title">
-                                    고객수
-                                </div>
+                                <div className="table_title">고객수</div>
                                 <div className="table_contents w100">
-                                    <div className='table_inner_text'>0</div>
+                                    <div className="table_inner_text">0</div>
                                 </div>
                             </div>
                             <div className="table_section half">
-                                <div className="table_title">
-                                    상담희망수
-                                </div>
+                                <div className="table_title">상담희망수</div>
                                 <div className="table_contents w100">
-                                    <div className='table_inner_text'>0</div>
+                                    <div className="table_inner_text">0</div>
                                 </div>
                             </div>
                         </div>
-
                     </div>
 
                     <div className="modal_footer_box">
-                        <div className="modal_btn" onClick={handleSubmit}>
+                        <div className="modal_btn" onClick={() => handleUpdate()}>
                             수정
                         </div>
                         <div className="modal_btn close" onClick={() => deleteMember()}>
