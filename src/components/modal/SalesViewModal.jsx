e9cabad458673ce16ea3af0ed7b056194e98c974
devhong97 <devhong97@gmail.com>
2024-03-14 11:50:54 +0900

    20240314

--- src/components/modal/SalesViewModal.jsx
+++ src/components/modal/SalesViewModal.jsx
@@ -1,213 +1,211 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const SalesViewModal = (props) => {
-    const [detailNum, setDetailNum] = useState("");
-    useEffect(() => {
-        if (props.detailIdx) {
-            console.log(props.detailIdx);
-            setDetailNum(props.detailIdx);
-            getDetail();
-        }
-    }, [props.detailIdx])
-    const clearModal = () => {
-        props.closeModal()
+  const [detailNum, setDetailNum] = useState("");
+  useEffect(() => {
+    if (props.detailIdx) {
+      console.log(props.detailIdx);
+      setDetailNum(props.detailIdx);
+      getDetail();
     }
-    const getDetail = () => {
-        //detailNum 사용하여 상세 api 호출
-    }
-    const handleSubmit = () => {
-
-    }
-    return (
-        <div className="modal_wrap">
-            <div className="modal_back">
-                <div className="modal_box">
-                    <div className="modal_title_box">
-                        <div className="modal_title">매출 상세</div>
-                        <div className="modal_close_btn" onClick={() => clearModal()}>
-                            X
-                        </div>
-                    </div>
-                    <div className="table_box">
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    이름<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    연락처<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    가입일<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    영업자<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    상품명<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    병원<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    희망일1<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    희망일2<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    희망일3<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    검진유무<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="table_row">
-                            <div className="table_section">
-                                <div className="table_title">
-                                    상담희망<p className="title_point">*</p>
-                                </div>
-                                <div className="table_contents w100">
-                                    <input
-                                        className="table_input modal"
-                                        type="text"
-                                        id="title"
-                                        placeholder="지점명을 입력해주세요."
-                                    ></input>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="modal_footer_box">
-                        <div className="modal_btn" onClick={handleSubmit}>
-                            수정
-                        </div>
-                        <div className="modal_btn close" onClick={clearModal}>
-                            닫기
-                        </div>
-                    </div>
-                </div>
+  }, [props.detailIdx]);
+  const clearModal = () => {
+    props.closeModal();
+  };
+  const getDetail = () => {
+    //detailNum 사용하여 상세 api 호출
+  };
+  const handleSubmit = () => {};
+  return (
+    <div className="modal_wrap">
+      <div className="modal_back">
+        <div className="modal_box">
+          <div className="modal_title_box">
+            <div className="modal_title">매출 상세</div>
+            <div className="modal_close_btn" onClick={() => clearModal()}>
+              X
             </div>
+          </div>
+          <div className="table_box">
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  이름<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  연락처<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  가입일<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  영업자<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  상품명<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  병원<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  희망일1<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  희망일2<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  희망일3<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  검진유무<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="table_row">
+              <div className="table_section">
+                <div className="table_title">
+                  상담희망<p className="title_point">*</p>
+                </div>
+                <div className="table_contents w100">
+                  <input
+                    className="table_input modal"
+                    type="text"
+                    id="title"
+                    placeholder="지점명을 입력해주세요."
+                  ></input>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="modal_footer_box">
+            <div className="modal_btn" onClick={handleSubmit}>
+              수정
+            </div>
+            <div className="modal_btn close" onClick={clearModal}>
+              닫기
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SalesViewModal;
